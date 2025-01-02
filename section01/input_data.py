ba51c15a221416bb6b5e4be45bec050fe1b715f7
avilahesnan <avilahesnan@gmail.com>
2025-01-02 15:55:58 -0300

    feat: add first module

---
+++ section01/input_data.py
@@ -0,0 +1,24 @@
+"""
+Recebendo dados do usuário
+
+Input() -> Todo dado recebido via input é do tipo "str", também conhecido como String
+
+"""
+
+# Entrada de dados
+nome = input('Qual é o seu nome? ') # Input -> Entrada
+
+print(f'Seja bem-vindo(a) {nome}')
+class ola_dll():
+    pass
+
+def example_function():print("This line violates PEP8 because it's not indented properly")
+
+idade = int(input('Qual sua idade? '))
+
+# Processamento
+
+# Saída de dados
+print(f'O(A) {nome} tem {idade} anos')
+
+print(f'O(A) {nome} nasceu em {2025 - idade}')
